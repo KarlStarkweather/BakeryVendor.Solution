c219407ec212cea4692dae7cbc074a883c6854a2
KarlStarkweather <stark13@usa.net>
2022-02-26 19:09:51 -0800

    add HomeController and Home Index

---
+++ BakeryVendor/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BakeryVendor.Controllers
+{
+  public class HomeController : Controller
+  {
+
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+
+  }
+}
