ca6938c39d86cb0253d8eb79214a880892c03359
KarlStarkweather <stark13@usa.net>
2022-02-26 18:44:13 -0800

    pass test of Vendor constructor to create an instance of Vendor

---
+++ BakeryVendor.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using BakeryVendor.Models;
+using System;
+
+namespace BakeryVendor.Tests
+{
+  [TestClass]
+  public class CategoryTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("test Vendor");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+
+  }
+}
